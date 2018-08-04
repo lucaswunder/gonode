f3f5af9b9177275461ad241609a7302abc700945
Lucas Wundervald <wundervald_2@hotmail.com>
2018-08-03 23:42:30 -0300

    Cadastro de categorias

--- App02/app/routes.js
+++ App02/app/routes.js
@@ -2,26 +2,47 @@ const express = require('express');
 
 const routes = express.Router();
 
+/**
+ * middlewares
+ */
 const authMiddleware = require('./middlewares/auth');
 const guestMiddleware = require('./middlewares/guest');
 
+/**
+ * Controllers
+ */
 const authController = require('./controllers/authController');
 const dashboardController = require('./controllers/dashboardController');
+const categoryController = require('./controllers/categoryController');
 
+/**
+ * Flash messages
+ */
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success');
   res.locals.flashError = req.flash('error');
   next();
 });
 
+/**
+ * Auth
+ */
 routes.get('/', guestMiddleware, authController.signin);
 routes.get('/signup', guestMiddleware, authController.sigup);
 routes.post('/register', authController.register);
 routes.post('/authenticate', authController.authenticate);
 routes.get('/signout', authController.signout);
 
+/**
+ * Dashboard
+ */
 routes.use('/app', authMiddleware);
 routes.get('/app/dashboard', dashboardController.index);
+
+/**
+ * Category
+ */
+routes.post('/app/categories/create', categoryController.store);
 
 routes.use((req, res) => res.render('errors/404'));
 
