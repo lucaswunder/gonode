5608ea81ab1bb8ba2461648b7e5a01f0580e4735
Lucas Wundervald <wundervald_2@hotmail.com>
2018-08-06 11:34:01 -0300

    Listagem de snippets

--- App02/app/controllers/categoryController.js
+++ App02/app/controllers/categoryController.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models');
+const { Category, Snippet } = require('../models');
 
 module.exports = {
   async store(req, res, next) {
@@ -7,6 +7,25 @@ module.exports = {
       req.flash('success', 'Categoria criada');
 
       return res.redirect(`/app/categories/${category.id}`);
+    } catch (err) {
+      return next(err);
+    }
+  },
+
+  async show(req, res, next) {
+    try {
+      const categories = await Category.findAll({
+        include: [Snippet],
+        where: {
+          UserId: req.session.user.id,
+        },
+      });
+
+      const snippets = await Snippet.findAll({
+        where: { CategoryId: req.params.id },
+      });
+
+      return res.render('categories/show', { categories, snippets, activeCat: req.params.id });
     } catch (err) {
       return next(err);
     }
--- App02/app/models/snippet.js
+++ App02/app/models/snippet.js
@@ -1,8 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
-  const Snippet = sequelize.define('Snippet', {
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-  });
+  const Snippet = sequelize.define(
+    'Snippet',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+    },
+    {
+      getterMethods: {
+        excerpt() {
+          return this.content.lenght > 120
+            ? `${this.content.substring(0, this.content.lastIndexOf(' ', 120))}...`
+            : this.content;
+        },
+      },
+    },
+  );
 
   Snippet.associate = (models) => {
     Snippet.belongsTo(models.Category);
